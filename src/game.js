f8789ba274f2c98c818d7327988440d3765817ff
Jim Charnesky <mijnesky@hotmail.com>
2021-03-04 15:26:18 -0700

    Add properties with array values to Game class

--- src/game.js
+++ src/game.js
@@ -1,7 +1,17 @@
 class Game {
   constructor() {
-    this.player1 = new Player()
-    this.player2 = player;
-    this.turn = true;
+    this.player1 = new Player('One', 🍩);
+    this.player2 = new Player('Two', 🥓);
+    this.turn = true; // or [player1, player2]??
+    this.tokenGameSpace = [
+       boxA, boxB, boxC,
+       boxD, boxE, boxF,
+       boxG, boxH, boxI
+    ];
+    this.gameWinConditions = [
+      'abc','def','ghi',
+      'adg', 'beh','cfi',
+      'aei', 'ceg'
+    ];
   }
 }
