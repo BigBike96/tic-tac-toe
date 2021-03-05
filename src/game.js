29557611815275df57e38706ea9a9437e26e7f19
Jim Charnesky <mijnesky@hotmail.com>
2021-03-05 11:19:55 -0700

    Add for loop and if statment to changeGameSpaceData

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 class Game {
   constructor() {
-    this.player1 = new Player('One', '🍩');
-    this.player2 = new Player('Two', '🥓');
+    this.player1 = new Player('One', '🥓');
+    this.player2 = new Player('Two', '🍩');
     this.turn = true; // or [player1, player2]??
     this.tokenGameSpace = [
        boxA, boxB, boxC,
@@ -14,7 +14,18 @@ class Game {
       'aei', 'ceg'
     ];
   }
-  
+
+  changeGameSpaceData(event) {
+    for (var i = 0; i < this.tokenGameSpace.length) {
+    if (this.turn && this.target.id === this.tokenGameSpace[i]) {
+      this.tokenGameSpace.splice(i, 1);
+      //whatever is clicked, update
+    } else if (!this.turn) {
+
+    }
+    }
+  }
+
   detectWinConditions() {
 
   }
