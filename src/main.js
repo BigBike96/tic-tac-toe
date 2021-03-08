f0a72775de81d9e273d37a805a09765445d084d9
Jim Charnesky <mijnesky@hotmail.com>
2021-03-07 17:27:50 -0700

    Add variables assigned to querySelectors

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 // main.js file
+var boxA = document.querySelector('#boxA');
+var boxB = document.querySelector('#boxA');
+var boxC = document.querySelector('#boxA');
+var boxD = document.querySelector('#boxA');
+var boxE = document.querySelector('#boxA');
+var boxF = document.querySelector('#boxA');
+var boxG = document.querySelector('#boxA');
+var boxH = document.querySelector('#boxA');
+var boxI = document.querySelector('#boxA');
 
 // function runs on each click
 // figure out which space was clicked
