5b80969cdbe9010c5385ff5aac7f92d6037a4426
Jim Charnesky <mijnesky@hotmail.com>
2021-03-08 11:43:59 -0700

    Add function to reset the game board on the DOM

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var player1Container = document.querySelector('#player1Container');
 var player2Container = document.querySelector('#player2Container');
 var turnIndicator = document.querySelector('#turnIndicator');
 var gamePlayArea = document.querySelector('#gamePlayArea');
+// var selectAllGameArea = document.querySelectorAll('')
 var boxA = document.querySelector('#boxA');
 var boxB = document.querySelector('#boxB');
 var boxC = document.querySelector('#boxC');
@@ -15,44 +16,68 @@ var boxI = document.querySelector('#boxI');
 var currentGame = new Game();
 
 // window.addEventListener('load', storageGameboard);
-// gamePlayArea.addEventListener('click', clickHandler);
-gamePlayArea.addEventListener('click', gameBoardAction);
+gamePlayArea.addEventListener('click', clickHandler);
+// gamePlayArea.addEventListener('click', gameBoardAction);
 
-// function clickHandler(event) {
-//   event.preventDefault();
-//   if (event.target.classList.contains('a')) {
-//     currentGame.changeGameSpaceData(boxA);
-//   } else if ((event.target.classList.contains('b'))) {
-//     console.log('b');
-//   } else if ((event.target.classList.contains('c'))) {
-//     console.log('c');
-//   } else if ((event.target.classList.contains('d'))) {
-//     console.log('d');
-//   } else if ((event.target.classList.contains('e'))) {
-//
-//   } else if ((event.target.classList.contains('f'))) {
-//
-//   } else if ((event.target.classList.contains('g'))) {
-//
-//   } else if ((event.target.classList.contains('h'))) {
-//
-//   } else if ((event.target.classList.contains('i'))) {
-//
-//   }
-// }
+function clickHandler(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('a')) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxA.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('b'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxB.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('c'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxC.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('d'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxD.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('e'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxE.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('f'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxF.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('g'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxG.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('h'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxH.innerText = currentGame.currentPlayer.token;
+  } else if ((event.target.classList.contains('i'))) {
+    currentGame.changeGameSpaceData(event.target.id);
+    boxI.innerText = currentGame.currentPlayer.token;
+  }
+  // checkDOMGameData();
+}
+
+function clearScreenBoard() {
+  boxA.innerText = "";
+  boxB.innerText = "";
+  boxC.innerText = "";
+  boxD.innerText = "";
+  boxE.innerText = "";
+  boxF.innerText = "";
+  boxG.innerText = "";
+  boxH.innerText = "";
+  boxI.innerText = "";
+}
 // gameBoardAction();
 
-function gameBoardAction(event) {
-  event.preventDefault();
-  for (var i = 0; i < currentGame.tokenGameSpaces.length; i++) {
-    if (event.target.id === currentGame.tokenGameSpaces[i].id) {
-      console.log('pants', event.target.id);
-      console.log(currentGame);
-      currentGame.changeGameSpaceData(event.target.id);
-      console.log(currentGame);
-    }
-  }
-}
+// function gameBoardAction(event) {
+//   event.preventDefault();
+//   var clickedBox = event.target.id
+//   for (var i = 0; i < currentGame.tokenGameSpaces.length; i++) {
+//     if (event.target.id === currentGame.tokenGameSpaces[i].id) {
+//       currentGame.changeGameSpaceData(event.target.id);
+//       console.log(clickedBox);
+//       clickedBox.innerText = currentGame.currentPlayer.token;
+//     }
+//   }
+// }
+
+// function
 // function runs on each click
 // figure out which space was clicked
 // changeGameSpaceData
