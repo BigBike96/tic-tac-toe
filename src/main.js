e6798b306d7806a5c221d3a35ecae94b3ec4ac21
Jim Charnesky <mijnesky@hotmail.com>
2021-03-08 08:50:47 -0700

    Add click handler function for game board

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 var player1Container = document.querySelector('#player1Container');
 var player2Container = document.querySelector('#player2Container');
+var turnIndicator = document.querySelector('#turnIndicator');
 var gamePlayArea = document.querySelector('#gamePlayArea');
 var boxA = document.querySelector('#boxA');
 var boxB = document.querySelector('#boxB');
@@ -11,17 +12,45 @@ var boxG = document.querySelector('#boxG');
 var boxH = document.querySelector('#boxH');
 var boxI = document.querySelector('#boxI');
 
-var game = new Game();
+var currentGame = new Game();
 
-gamePlayArea.addEventListener(clickHandler);
+// window.addEventListener('load', storageGameboard);
+gamePlayArea.addEventListener('click', clickHandler);
+// gamePlayArea.addEventListener('click', gameBoardAction);
 
 function clickHandler(event) {
   event.preventDefault();
-  if (event.target.id === boxA) {
-    console.log('pants');
+  if (event.target.classList.contains('a')) {
+    console.log('a');
+  } else if ((event.target.classList.contains('b'))) {
+    console.log('b');
+  } else if ((event.target.classList.contains('c'))) {
+    console.log('c');
+  } else if ((event.target.classList.contains('d'))) {
+    console.log('d');
+  } else if ((event.target.classList.contains('e'))) {
+
+  } else if ((event.target.classList.contains('f'))) {
+
+  } else if ((event.target.classList.contains('g'))) {
+
+  } else if ((event.target.classList.contains('h'))) {
+
+  } else if ((event.target.classList.contains('i'))) {
 
   }
 }
+// gameBoardAction();
+//
+// function gameBoardAction(event) {
+//   for (var i = 0; i < currentGame.tokenGameSpaces.length; i++) {
+//     if (event.target.id === currentGame.tokenGameSpaces[i]) {
+//       console.log('pants');
+//       currentGame.changeGameSpaceData(event);
+//
+//     }
+//   }
+// }
 // function runs on each click
 // figure out which space was clicked
 // changeGameSpaceData
