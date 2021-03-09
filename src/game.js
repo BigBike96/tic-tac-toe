5772262f42786a4cd4df904bd239827f186d22b7
Jim Charnesky <mijnesky@hotmail.com>
2021-03-09 13:25:38 -0700

    Refactor detectWinConditions method

--- src/game.js
+++ src/game.js
@@ -37,43 +37,34 @@ class Game {
   detectWinConditions() {
     for (var i = 0; i < this.tokenGameSpaces.length; i++) {
       if (this.tokenGameSpaces[0] === this.tokenGameSpaces[1] && this.tokenGameSpaces[1] === this.tokenGameSpaces[2] && this.tokenGameSpaces[2] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[3] === this.tokenGameSpaces[4] && this.tokenGameSpaces[4] === this.tokenGameSpaces[5] && this.tokenGameSpaces[5] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[6] === this.tokenGameSpaces[7] && this.tokenGameSpaces[7] === this.tokenGameSpaces[8] && this.tokenGameSpaces[8] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[0] === this.tokenGameSpaces[3] && this.tokenGameSpaces[3] === this.tokenGameSpaces[6] && this.tokenGameSpaces[6] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[1] === this.tokenGameSpaces[4] && this.tokenGameSpaces[4] === this.tokenGameSpaces[7] && this.tokenGameSpaces[7] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[2] === this.tokenGameSpaces[5] && this.tokenGameSpaces[5] === this.tokenGameSpaces[8] && this.tokenGameSpaces[8] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[0] === this.tokenGameSpaces[4] && this.tokenGameSpaces[4] === this.tokenGameSpaces[8] && this.tokenGameSpaces[8] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.tokenGameSpaces[2] === this.tokenGameSpaces[4] && this.tokenGameSpaces[4] === this.tokenGameSpaces[6] && this.tokenGameSpaces[6] === this.currentPlayer.id) {
-        this.currentPlayer.addWin();
-        this.resetGameBoard();
-        this.currentPlayer.saveWinsToStorage();
+        this.updateDataToReflectWin();
       } else if (this.turnCounter === 9) {
         this.resetGameBoard();
         this.currentPlayer.saveWinsToStorage();
       }
     }
   }
+
+  updateDataToReflectWin() {
+    this.currentPlayer.addWin();
+    this.resetGameBoard();
+    this.currentPlayer.saveWinsToStorage();
+  }
+
 
   resetGameBoard() {
     this.tokenGameSpaces = [
